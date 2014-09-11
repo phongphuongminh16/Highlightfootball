e09d4c8977275e2e4288e111cd0e4890d7fbe1ae
Quangminhs <quangminhs@gmail.com>
2014-09-11 17:12:41 +0700

    Test lai

--- gen/edu/niitict/higlightfootball/R.java
+++ gen/edu/niitict/higlightfootball/R.java
@@ -67,16 +67,17 @@ public final class R {
         public static final int list_selector=0x7f02000c;
     }
     public static final class id {
-        public static final int action_search=0x7f0b0009;
-        public static final int counter=0x7f0b0005;
+        public static final int action_search=0x7f0b000a;
+        public static final int counter=0x7f0b0006;
         public static final int drawer_layout=0x7f0b0000;
         public static final int frame_container=0x7f0b0001;
-        public static final int icon=0x7f0b0003;
+        public static final int icon=0x7f0b0004;
         public static final int list_slidermenu=0x7f0b0002;
-        public static final int pager=0x7f0b0007;
-        public static final int pager_tab_strip=0x7f0b0008;
-        public static final int title=0x7f0b0004;
-        public static final int txtLabel=0x7f0b0006;
+        public static final int pager=0x7f0b0008;
+        public static final int pager_tab_strip=0x7f0b0009;
+        public static final int search_btSearch=0x7f0b0003;
+        public static final int title=0x7f0b0005;
+        public static final int txtLabel=0x7f0b0007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
